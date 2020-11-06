ee500f917432265c5b08047ebbdca2224596ab90
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-11-06 19:23:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# eth-indexer-service
+
+Runs a eth-transaction-indexer and logs to a Hyperbee.
+
+## Usage
+
+Start the service with:
+
+```
+node index -p <PORT> -d <DIRECTORY>
+
+> index stored at: <FEED_KEY>
+```
+
+Data will be a logged to a hypercore with the given FEED_KEY.
+
+To add an Ethereum address to be indexed, send a POST request of the form: 
+
+```sh
+curl -X POST http://localhost:8080/add?addr=0xde...adbeef
+```
+
+
---
+++ index.js
@@ -0,0 +1,69 @@
+const http = require('http')
+const url = require('url')
+const replicate = require('@hyperswarm/replicator')
+const hypercore = require('hypercore')
+const Indexer = require('@hyperdivision/eth-transaction-indexer')
+const minimist = require('minimist')
+
+var args = minimist(process.argv.slice(2), {
+  string: ['p', 'd', 'h'],
+  alias: { h: 'help', p: 'port', d: 'directory' },
+  unknown: function () {
+    console.log('unrecognised command.')
+    process.exit(1)
+  }
+})
+
+const port = parseInt(args.p) || 8080
+const dir = args.d || './ropsten-index'
+
+const testnet = 'https://ropsten.infura.io/v3/2aa3f1f44c224eff83b07cef6a5b48b5'
+const feed = new hypercore(dir)
+
+const index = new Indexer(testnet, feed)
+const since = 0x899b00 - 200
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    const u = new url.URL(req.url, 'http://dummyurl:0000')
+
+    switch (u.pathname) {
+      case '/add' :
+        const address = u.searchParams.get('addr')
+
+        index.add(address)
+        console.log('now tracking: ' + address)
+
+        res.end('ok')
+        return
+
+      default :
+        res.end()
+        return
+    }
+  }
+})
+
+process.on('SIGINT', stop)
+process.on('SIGTERM', stop)
+
+let swarm
+feed.ready(async () => {
+  index.start(since)
+
+  console.log('index stored at:', feed.key.toString('hex'))
+
+  server.listen(port)
+  const swarm = replicate(feed, { lookup: true, announce: true })
+
+  feed.on('close', err => {
+    if (err) throw err
+    swarm.destroy()
+  })
+})
+
+async function stop () {
+  server.close()
+  await index.stop()
+  feed.close()
+}
